440c4d7658efb65fb924dd53a362dfa4e7df5a52
Steve Sparks <sparks@bignerdranch.com>
2013-11-26 12:51:00 -0500

    editing script

--- git.js
+++ git.js
@@ -1,5 +1,5 @@
 function b() {
-    var x=document.body.innerHTML.replace(/(\d\d\d\d\d\d\d\d)/g,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>"));
+    var x=document.body.innerHTML.replace('/(\d\d\d\d\d\d\d\d)/g',"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>");
     document.body.innerHTML=x;
 }
 alert('Trying');
