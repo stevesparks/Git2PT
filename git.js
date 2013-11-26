4ca9b0c464045122edf738c7477b81d4e7f05c01
Steve Sparks <sparks@bignerdranch.com>
2013-11-26 12:47:13 -0500

    nulls it

--- git.js
+++ git.js
@@ -1,4 +1,3 @@
-function b(){var x=document.body.innerHTML.replace(/(\d\d\d\d\d\d\d\d)/g,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>"));document.body.innerHTML=x;}
-b();
+
 
 
