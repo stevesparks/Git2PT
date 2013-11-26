2ac98285c54f1a95d6a2b39e654e92a8dd9f2946
Steve Sparks <sparks@bignerdranch.com>
2013-11-26 12:56:02 -0500

    RE fix

--- git.js
+++ git.js
@@ -1,5 +1,5 @@
 function b() {
-    var RE = new RegExp('(\d\d\d\d\d\d\d\d)','g');
+    var RE = new RegExp('Fixes #(\d\d\d\d\d\d\d\d)','g');
     var x=document.body.innerHTML.replace(RE,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>");
     document.body.innerHTML=x;
     alert('Replaced');
