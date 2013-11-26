8e23285d2bac57c03fce6c0883c0a9f352970319
Steve Sparks <sparks@bignerdranch.com>
2013-11-26 12:49:35 -0500

    adds some fund

--- git.js
+++ git.js
@@ -1,3 +1,9 @@
+function b() {
+    var x=document.body.innerHTML.replace(/(\d\d\d\d\d\d\d\d)/g,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>"));
+    document.body.innerHTML=x;
+}
+alert('Trying');
+//b();
 
 
 
