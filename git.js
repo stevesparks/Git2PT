5484f2e66117fb2569df4023f99ec5ba106974d4
Steve Sparks <sparks@bignerdranch.com>
2013-11-26 13:03:14 -0500

    RE work

--- git.js
+++ git.js
@@ -1,5 +1,5 @@
 function b() {
-    var RE = new RegExp('Fixes #(\d\d\d\d\d\d\d\d)','g');
+    var RE = /Fixes #(\d\d\d\d\d\d\d\d)/g;
     var x=document.body.innerHTML.replace(RE,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>");
     document.body.innerHTML=x;
     console.log(x);
