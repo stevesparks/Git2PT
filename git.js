831712d7c21d2e0eee566b12e31772e93af6f56c
Steve Sparks <sparks@bignerdranch.com>
2013-11-26 12:55:01 -0500

    alerts

--- git.js
+++ git.js
@@ -2,6 +2,7 @@ function b() {
     var RE = new RegExp('/(\d\d\d\d\d\d\d\d)','g');
     var x=document.body.innerHTML.replace(RE,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>");
     document.body.innerHTML=x;
+    alert('Replaced');
 }
 b();
 
