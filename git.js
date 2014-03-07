b5f689c212fe81383152736670b056c08e8d247b
Steve Sparks <sparks@bignerdranch.com>
2014-03-07 08:56:09 -0500

    removes alerts

--- git.js
+++ git.js
@@ -3,14 +3,12 @@ function a() {
     var x=document.body.innerHTML.replace(RE,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>");
     document.body.innerHTML=x;
     console.log(x);
-    alert('Replaced');
 }
 function b() {
     var RE = /Fixes #(\d\d\d\d\d\d\d\d)/g;
     var x=document.body.innerHTML.replace(RE,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>");
     document.body.innerHTML=x;
     console.log(x);
-    alert('Replaced');
 }
 a();
 b();
