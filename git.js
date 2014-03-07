13682dc1674a6a8ec9197e73e0fab9aef8f23eec
Steve Sparks <sparks@bignerdranch.com>
2014-03-07 08:55:24 -0500

    catches 'Finishes' in a most brutal way

--- git.js
+++ git.js
@@ -1,3 +1,10 @@
+function a() {
+    var RE = /Finishes #(\d\d\d\d\d\d\d\d)/g;
+    var x=document.body.innerHTML.replace(RE,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>");
+    document.body.innerHTML=x;
+    console.log(x);
+    alert('Replaced');
+}
 function b() {
     var RE = /Fixes #(\d\d\d\d\d\d\d\d)/g;
     var x=document.body.innerHTML.replace(RE,"<a href='http://www.pivotaltracker.com/story/show/$1' window=_new>$&</a>");
@@ -5,6 +12,7 @@ function b() {
     console.log(x);
     alert('Replaced');
 }
+a();
 b();
 
 
